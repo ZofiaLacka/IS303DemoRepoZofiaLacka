a0899f3b35a28da9e423a45b5badfed438c71216
ZofiaLacka <zofia.lacka.bb@gmail.com>
2026-03-03 13:35:51 -0700

    Create multiplyTwoNumbers function
    Wrote a function that accepts two numbers and multiplies

---
+++ multiply.py
@@ -0,0 +1,7 @@
+# Zofia Lacka
+# 03/03/2026
+# Description: Accept 2 numbers and return the product
+def multiplyTwoNumbers(iNum1, iNum2):
+    # Multiply and return the result
+    return iNum1 * iNum2
+#end
